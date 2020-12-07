b5317a88ac0306e19adaff800d6cb9341fa1c3a7
Charles Reynolds <reynoldscem@gmail.com>
2020-12-07 13:02:31 +0000

    Day 6 part 2

---
+++ day_06/2.py
@@ -0,0 +1,24 @@
+from argparse import ArgumentParser
+
+
+def build_parser():
+    parser = ArgumentParser()
+
+    parser.add_argument('input_filename')
+
+    return parser
+
+
+def main():
+    args = build_parser().parse_args()
+    with open(args.input_filename) as fd:
+        data = fd.read().strip()
+    groups = data.split('\n\n')
+    counts = [
+        len(set.intersection(*map(set, group.split())))
+        for group in groups
+    ]
+    print(sum(counts))
+
+if __name__ == '__main__':
+    main()
